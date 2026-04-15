8dbd2f7bb779d434d95aeaf18bd7593d594378fc
gayathri1310 <66237216+gayathri1310@users.noreply.github.com>
2026-04-15 16:45:13 +0000

    combine commit

--- docker/ares/frontEnd/app.js
+++ docker/ares/frontEnd/app.js
@@ -5,10 +5,11 @@ var path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const URL = process.env.URL || 'http://localhost:4000/api';
+const URL = 'http://localhost:4000/api' || process.env.URL;
 
 app.get('/', async function(req, res) {
     try {
+        
         const response = await fetch(URL, { method: 'GET' });
         const data = await response.json();
         res.render('index', { data: data });
